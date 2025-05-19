221cfb5d16cfd2d17bfef513477b250a3fa909fd
Jefferson Vinícius <vinicius.felipe1319@gmail.com>
2025-05-19 20:34:31 -0300

    Tabuada

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,19 @@
+function tabuada() 
+{
+    var num = document.getElementById('txtnum').value
+    var res = document.getElementById('res')
 
+    res.innerHTML = ""
+    if (num == '')
+    {
+        alert('Por favor, digite um número!')
+    } else 
+    {
+        num = Number(num)
+        for (var i = 1; i <= 10; i++) 
+        {
+            res.innerHTML += `${num} x ${i} = ${num * i} \n`
+        }
+    }
+    
+}
