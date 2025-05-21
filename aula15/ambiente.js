c15606f1a72f0e1d5c361136d309d1780289a8f2
Jefferson Vinícius <vinicius.felipe1319@gmail.com>
2025-05-20 22:11:23 -0300

    aula15

---
+++ aula15/ambiente.js
@@ -0,0 +1,19 @@
+let num = [5, 8, 2, 9, 3]
+num.push(6)
+num.sort()
+let pos = num.indexOf(10)
+
+if (pos == -1)
+{
+    console.log(`O número não existe`)
+}
+
+/* for (var i = 0; i < num.length; i++)
+{
+    console.log(num[i])
+} */
+
+    /*  for (var i in num)
+    {
+       console.log(num[i])
+    } */
